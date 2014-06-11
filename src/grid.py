d5176db9e6d86208a0f210be6c4e3b43e3e473d5
Cooper LeBrun <clebrun@localhost.localdomain>
2014-06-10 17:48:19 -0700

    forgot to add grid, still unrusting here :P

--- src/grid.py
+++ src/grid.py
@@ -7,12 +7,12 @@ class Grid(nx.DiGraph):
 
     def __init__(self):
         """
-        Could be modified to take any size Grid?
         1. Checks input
         2. Makes Graph from input
         """
-        self.SIZE = (4, 4)
+        self.SIZE = (4, 4) # could be any size but doesn't need to
         super().__init__(self)
 
+        # Is it ok to just have 1-16 nodes or are coords needed?
         for i in range (0, self.SIZE[0] * self.SIZE[1]):
-            self.add_node(i)    
+            self.add_node(i)
--- src/grid_test.py
+++ src/grid_test.py
@@ -3,6 +3,10 @@ from grid import Grid
 
 class GridTests(unittest.TestCase):
 
+    def testSetsNodeAttributes(self):
+        test_attrs = [ x for x in range(0, 16) ] # 0 - 15
+        self.g = Grid(test_attrs)
+
     def setUp(self):
         self.g = Grid()
 
@@ -12,4 +16,6 @@ class GridTests(unittest.TestCase):
     def testHasAppropriateNumberOfNodes(self):
         self.assertEqual(len(self.g.nodes()), 16)
 
+
+    # do we need coord style nodes?
     #def testForGridStyleEdges(self):
