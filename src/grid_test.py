eb0d85346e14f690a2fe5a98957ae030b6643e89
Cooper Lebrun <cooperlebrun@gmail.com>
2014-06-11 15:06:19 -0700

    test_attrs statement unneeded

--- src/grid_test.py
+++ src/grid_test.py
@@ -4,14 +4,12 @@ from grid import Grid
 class NodeTests(unittest.TestCase):
 
     def testSetsNodeAttributes(self):
-        test_attrs = [ x for x in range(0, 16) ] # 0 - 15
-        self.g = Grid(test_attrs)
+        self.g = Grid([ x for x in range(0, 16) ])
         for x in range(0, 16):
             self.assertEqual(self.g.node[x]['value'], x)
 
     def setUp(self):
-        test_attrs = [ x for x in range(0, 16) ] # 0 - 15
-        self.g = Grid(test_attrs)
+        self.g = Grid([ x for x in range(0, 16) ])
 
     def testHasAppropriateNumberOfNodes(self):
         self.assertEqual(len(self.g.nodes()), 16)
