44c441c7542bb8a7977fc5f3bbbfae833bd29e7b
Cooper Lebrun <cooperlebrun@gmail.com>
2014-06-10 18:29:05 -0700

    updated docs, tweaked indentation

--- src/grid.py
+++ src/grid.py
@@ -3,15 +3,14 @@
 import networkx as nx
 
 class Grid(nx.DiGraph):
-    # responsible for making a graph in the structure of a grid and filling it with values, but not enforcing rules.
+  """
+  Wraps nx.DiGraph
+  Handles initializing nodes, values and edges in 4x4 grid manner.
+  """
 
-    def __init__(self, attributes):
-        """
-        1. Checks input
-        2. Makes Graph from input
-        """
-        self.SIZE = (4, 4) # could be any size but doesn't need to
-        super().__init__(self)
+  def __init__(self, attributes):
+    self.SIZE = (4, 4) # could be any size but doesn't need to
+    super().__init__(self)
 
-        # Is it ok to just have 1-16 nodes or are coords needed?
-        self.add_nodes_from(zip([x for x in range(0, 16)], attributes))
+    # Is it ok to just have 1-16 nodes or are coords needed?
+    self.add_nodes_from(zip([x for x in range(0, 16)], attributes))
