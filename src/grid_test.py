ef31b6cc27366f0d2163867b7b88b63734d0c47a
Cooper Lebrun <cooperlebrun@gmail.com>
2014-06-10 18:31:25 -0700

    tweaked SetsAttrs test a bit less annoying

--- src/grid_test.py
+++ src/grid_test.py
@@ -6,11 +6,11 @@ class GridTests(unittest.TestCase):
     def testSetsNodeAttributes(self):
         # not attributes in the networkx sense, just in the sense that the node object itself can store a value.
         test_attrs = [ x for x in range(0, 16) ] # 0 - 15
-        should_be = [(0,0), (1,1), (2,2), (3,3), (4,4), (5,5),
-                (6,6), (7,7), (8,8), (9,9), (10,10), (11, 11),
-                (12, 12), (13, 13), (14, 14), (15, 15)]
+        should_be = list(zip(test_attrs, test_attrs))
         self.g = Grid(test_attrs)
         self.assertEqual(sorted(self.g.nodes()), should_be)
+
+    #def testRejectsWrongLengthAttributes(self):
 
     def setUp(self):
         test_attrs = [ x for x in range(0, 16) ] # 0 - 15
@@ -22,6 +22,4 @@ class GridTests(unittest.TestCase):
     def testHasAppropriateNumberOfNodes(self):
         self.assertEqual(len(self.g.nodes()), 16)
 
-
-    # do we need coord style nodes?
     #def testForGridStyleEdges(self):
