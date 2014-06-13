daf5d38ef3327be7f17078a42e8af5ddaf4190dc
Cooper Lebrun <cooperlebrun@gmail.com>
2014-06-12 23:30:21 -0700

    Score class done

--- src/score.py
+++ src/score.py
@@ -1,2 +1,27 @@
+from functools import reduce
+
 class Score:
-    pass
+
+    def __init__(self, init_score):
+        self.total = None
+        self.string = []
+        self.add(init_score)
+
+    def _combine(total, new):
+        if total is None or total == 0: # if first in the sequence
+            return new
+        elif new % 2 == 0: # if even
+            return total + new
+        elif new % 2 == 1: # if odd
+            return total * new
+
+    def _calculate_from(nums):
+        return reduce(Score._combine, nums)
+
+    def add(self, new):
+        self.total = Score._combine(self.total, new)
+        self.string.append(new)
+
+    def add_from(self, news):
+        for new in news:
+            self.add(new)
--- src/score_test.py
+++ src/score_test.py
@@ -2,4 +2,14 @@ import unittest
 from score import Score
 
 class ScoreTest(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.score = Score(0)
+
+    def testAddingWorks(self):
+        self.score.add_from([5, 5, 4])
+        self.assertEqual(self.score.total, 29)
+
+    def testKeepsTrackOfScoreString(self):
+        self.score.add_from([1, 2, 3, 4, 5])
+        self.assertEqual(self.score.string, [0, 1, 2, 3, 4, 5])
