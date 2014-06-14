e6dcf705f109a4bf31763bb9dd1dc0061290648f
Cooper Lebrun <cooperlebrun@gmail.com>
2014-06-13 20:41:16 -0700

    Cell doesn't serve any purpose

--- src/cell.py
+++
@@ -1,4 +0,0 @@
-from random import randrange
-class Cell:
-    def __init__(self, value):
-        self.value = value
--- src/cell_test.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from cell import *
-
-class CellTests(unittest.TestCase):
-    def testTakesValue(self):
-        x = Cell(9)
-        self.assertEqual(x.value, 9)
