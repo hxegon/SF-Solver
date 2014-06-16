976dcb507ec8085be0d1d8f3f30caaa6d30e3a8b
Cooper Lebrun <cooperlebrun@gmail.com>
2014-06-16 13:46:23 -0700

    added a couple todos

--- src/path.py
+++ src/path.py
@@ -5,11 +5,12 @@ class Path:
         self.history = []
 
     def goto(self, node):
+        ## good opportunity for optimizing
         if self.history == []:
             self.last = None
         else:
             self.last = self.history[-1]
         if node in self.history: # if node previously visited
-            raise Exception
+            raise Exception ## needs to be a custom exception
         self.current = node
         self.history.append(node)
