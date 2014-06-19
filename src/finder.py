8df25e0cbd0b30569182ad9000231b87727b3983
Cooper Lebrun <cooperlebrun@gmail.com>
2014-06-18 21:07:29 -0700

    changed find_paths_from to an iterative method

--- src/finder.py
+++ src/finder.py
@@ -4,10 +4,9 @@ class Finder:
         self.graph = graph
 
     def find_paths_from(self, start_point):
-
-        def recursive_search(stack, accumulator):
-            if not stack:
-                return accumulator
+        stack = [[start_point]]
+        accumulator = []
+        while stack:
             path = stack.pop()
             neighbors = self.graph.neighbors(path[-1])
             neighbors = [ n for n in neighbors if n not in path ] # Don't use the same node twice
@@ -16,8 +15,7 @@ class Finder:
             else:
                 for n in neighbors:
                     stack.append(path + [n])
-            return recursive_search(stack, accumulator)
-        return recursive_search([[start_point]], [])
+        return accumulator
 
     def find_all_paths(self):
         accumulator = [] # possibly not needed?
