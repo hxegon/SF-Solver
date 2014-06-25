f4be51b84d6fdb65a53518357e02a1936d0f8c56
Cooper Lebrun <cooperlebrun@gmail.com>
2014-06-24 19:51:10 -0700

    basic one puzzle I/O

---
+++ src/GUI.py
@@ -0,0 +1,30 @@
+#import sys
+from interface import *
+import tkinter as tk
+
+class MainApplication(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.PuzzleEntryLabel = tk.Label(self.parent, text="Enter Puzzle Numbers Here")
+        self.PuzzleEntryLabel.pack()
+        
+        self.PuzzleEntryBox = tk.Entry(self.parent, textvariable="NewPuzzle")
+        self.PuzzleEntryBox.pack()
+
+        self.PuzzleEntryButton = tk.Button(self.parent, text="Enter Puzzle", command=self.EnterPuzzle)
+        self.PuzzleEntryButton.pack()
+
+    def EnterPuzzle(self):
+        puzzle = self.PuzzleEntryBox.get()
+        if len(puzzle) == 16:
+            LabelText = str(scores_for(puzzle)[-1])
+        else:
+            LabelText = "Incorrect length. string was %s characters, when it needs to be 16 characters" % (len(puzzle))
+        PuzzleLabel = tk.Label(self.parent, text=LabelText)
+        PuzzleLabel.pack()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    MainApplication(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
