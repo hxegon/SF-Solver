c3d99ff0cf85d0c82108807c301715ef37a5867a
Cooper Lebrun <cooperlebrun@gmail.com>
2014-06-25 16:57:48 -0700

    added ShowPath, changed show method to ShowPath

--- src/GUI.py
+++ src/GUI.py
@@ -15,17 +15,24 @@ class MainApplication(tk.Frame):
         self.PuzzleEntryButton = tk.Button(self.parent, text="Enter Puzzle", command=self.EnterPuzzle)
         self.PuzzleEntryButton.pack()
 
+
     def EnterPuzzle(self):
         puzzle = self.PuzzleEntryBox.get()
         if len(puzzle) == 16:
-            scores = scores_for(puzzle)[-1]
+            scores = scores_for(puzzle)
             ScoreText = str(scores[-1][0])
-            PathText = str(scores[-1][1])
+            PathText = self.ShowPath(scores[-1][1])
             LabelText = "High Score: %s, Score Path: %s" % (ScoreText, PathText)
         else:
             LabelText = "Incorrect length. string was %s characters, when it needs to be 16 characters" % (len(puzzle))
         self.PuzzleLabel = tk.Label(self.parent, text=LabelText)
         self.PuzzleLabel.pack()
+
+    # changes a list of lists with (0,1) style grid coords to A2 style grid coords
+    def ShowPath(self, path):
+        coord_table = { 0: "A", 1: "B", 2: "C", 3: "D" }
+        return " ".join([ coord_table[x] + str(y+1) for x, y in path ])
+
 
 if __name__ == "__main__":
     root = tk.Tk()
